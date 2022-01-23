336a87732aafcab9605e260f0d9cca69163e93cb
Marcin Dabrowski <mdabrowskin@gmail.com>
2022-01-23 10:07:32 +0100

    Add reading additional properties of comments

--- scraper/main.py
+++ scraper/main.py
@@ -24,8 +24,12 @@ def get_data_from_subreddit(request):
     subreddit = request_json['subreddit']
     posts_to_scrape = request_json['posts_to_scrape']
 
-  comm_list = []
-  header_list = []
+  comments_list = []
+  titles_list = []
+  downs_list = []
+  ups_list = []
+  controversiality_list = []
+  awards_list = []
 
   for submission in reddit.subreddit(subreddit).hot(limit=posts_to_scrape):
     submission.comments.replace_more(limit=None)
@@ -33,24 +37,31 @@ def get_data_from_subreddit(request):
 
 
     while comment_queue:
-      header_list.append(submission.title)
+      titles_list.append(submission.title)
       comment = comment_queue.pop(0)
 
-      pprint.pprint(vars(comment))
+      comments_list.append(comment.body)
+      downs_list.append(comment.downs)
+      ups_list.append(comment.ups)
+      controversiality_list.append(comment.controversiality)
+      awards_list.append(len(comment.all_awardings))
 
-      comm_list.append(comment.body)
       t = []
       t.extend(comment.replies)
       while t:
-        header_list.append(submission.title)
+        titles_list.append(submission.title)
         reply = t.pop(0)
-        comm_list.append(reply.body)
+        comments_list.append(reply.body)
+        downs_list.append(reply.downs)
+        ups_list.append(reply.ups)
+        controversiality_list.append(reply.controversiality)
+        awards_list.append(len(reply.all_awardings))
 
-  header_list = [ftfy.fix_encoding(header) for header in header_list]
-  comm_list   = [ftfy.fix_encoding(comment) for comment in comm_list]
+  titles_list = [ftfy.fix_encoding(title) for title in titles_list]
+  comments_list = [ftfy.fix_encoding(comment) for comment in comments_list]
 
   file_name = generate_filename(subreddit)
-  df = wrap_data_in_data_frame(comm_list, header_list)
+  df = wrap_data_in_data_frame(titles_list, comments_list, downs_list, ups_list, controversiality_list, awards_list)
   upload_data_to_storage(df, file_name)
 
   return "DONE"
@@ -65,12 +76,19 @@ def upload_data_to_storage(df, file_name):
 
     blob.upload_from_string(df.to_csv(index=False))
 
-def wrap_data_in_data_frame(comm_list, header_list):
+def wrap_data_in_data_frame(titles_list, comments_list, downs_list, ups_list, controversiality_list, awards_list):
   import pandas
-  df = pandas.DataFrame(header_list)
-  df['comm_list'] = comm_list
-  df.columns = ['header','comments']
+  df = pandas.DataFrame(titles_list)
+
+  df['comm_list'] = comments_list
+  df['downs_list'] = downs_list
+  df['ups_list'] = ups_list
+  df['controversiality_list'] = controversiality_list
+  df['awards_list'] = awards_list
+
+  df.columns = ['header','comments', 'downs', 'ups', 'controversiality', 'awards']
   df['comments'] = df['comments'].apply(lambda x : x.replace('\n',''))
+  df['downs'] = df['downs'].apply(lambda x : x.replace('\n',''))
   return df
 
 def generate_filename(subreddit):
